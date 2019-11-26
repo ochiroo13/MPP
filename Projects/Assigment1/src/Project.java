29780f950a614ea1720299cdf51fb185ee5b78c0
Ochirsuren <ochiroo13@gmail.com>
2019-11-26 16:58:28 -0600

    Init first

---
+++ Projects/Assigment1/src/Project.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+
+public class Project {
+
+	private Date startDate;
+
+	private Date endDate;
+
+	private User scrumMaster;
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public User getScrumMaster() {
+		return scrumMaster;
+	}
+
+	public void setScrumMaster(User scrumMaster) {
+		this.scrumMaster = scrumMaster;
+	}
+
+}
---
+++ Projects/Assigment1/src/Tester.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class Tester extends User {
+
+	private Date birthDate;
+
+	private String firstName;
+
+	private String lastName;
+
+	private String gender;
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
